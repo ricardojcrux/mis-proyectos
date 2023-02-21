3159f1f1e124aff1a1f6e6b0980ee536ab4abfda
Ricardo Zapata <ricardozapatacruz@gmail.com>
2023-02-21 13:59:07 -0500

    optimizacion de dicc

--- dicc.py
+++ dicc.py
@@ -1,20 +1,17 @@
-persona={}
-continuar=True
+persona,decision={},'s'
+print('Bienvenido a la lista de compra\n')
 
-while continuar:
-	clave=input('¿Que datos quieres introducir? ')
-	valor=input(clave+' : ')
-	persona[clave] =valor
-	print(persona)
-	decision =input('¿Quieres añadir mas información (s/n)')
-
-	if decision=="n":
-		print('el programa se cierra')
+while True:
+	if decision == "n":
+		print('El programa se cierra')
 		break
-	elif decision=='s':
-		continuar=True
+	elif decision == 's':
+		clave = input('¿Que datos quieres introducir?: ')
+		valor = input(clave+' : ')
+		persona[clave] = valor
+		print(persona)
+		decision = input('¿Quieres añadir mas información (s/n): ')
 	else:
-		decision= input('Elegir entre si o no (s/n)')	
+		decision = input('Elegir entre si o no (s/n): ')	
 
-
-print('informacion de la persona', persona)
+print('informacion de la persona:', persona)
