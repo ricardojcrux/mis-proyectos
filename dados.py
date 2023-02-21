6b0f296b8795a56cc42106ced0ef3a0ece0e685e
Ricardo Zapata <ricardozapatacruz@gmail.com>
2023-02-20 23:35:29 -0500

    optimizacion de datos

--- dados.py
+++ dados.py
@@ -1,27 +1,17 @@
-import random
-import numpy as np
-import time
-import matplotlib.pyplot as plt
+import random, time, numpy as np, matplotlib.pyplot as plt
 
 print('Bienvenido a la calculadora de probabilidades!!!\n')
 x = int(input('Cuantas veces lanzaremos los dados: '))
 
 inic = time.time()
 lista = [random.randint(1,6) for i in range(x)]
-
+lista = sorted(lista)
 lista2 = dict(zip(lista,map(lambda i: lista.count(i),lista)))
+print(lista2)
 
-order = sorted(lista2)
-origin = lista2.keys()
-ordenado = {}
-
-for i in order:
-	ordenado[i] = lista2[i]
-
-print(ordenado)
 final = time.time()
 tot = final - inic
 print('El tiempo total del experimento es: ',tot)
 
-plt.bar(ordenado.keys(),list(ordenado.values()),color='red')
+plt.bar(lista2.keys(),list(lista2.values()),color='red')
 plt.show()
